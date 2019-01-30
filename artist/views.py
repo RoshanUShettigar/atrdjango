16db456d24e20a10710d6923d4516d7ba1a4a224
RoshanUShettigar <roshanind100@gmail.com>
2019-01-30 12:08:44 +0530

    modified views in artist

--- artist/views.py
+++ artist/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+
+class IndexView(generic.ListView):
+    pass
