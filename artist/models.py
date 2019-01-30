a3ec0e92d8547d916bf1c336199a9193245f7ebb
RoshanUShettigar <roshanind100@gmail.com>
2019-01-30 11:50:49 +0530

    my second commit 30-01-2019

---
+++ artist/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Artist(models.Model):
+   # Artist_id=models.AutoField()
+    Artist_name= models.CharField(max_length=50)
+    Email= models.EmailField(max_length=50)
+    Phone_no= models.IntegerField()
+
+    def __str__(self):
+        return self.Artist_name
+
+class Articles(models.Model):
+    Artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    Article_name=models.CharField(max_length=50)
+
